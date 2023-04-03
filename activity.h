03cf13694587f3d309ab102d66d448a8b323dc51
Remi Olateru <remiolateru@gmail.com>
2023-04-03 04:11:05 -0400

    Add comments to activity.h

--- activity.h
+++ activity.h
@@ -7,7 +7,8 @@
 
 
 
-//Enum through the differnt typ of emotions to be used in the Activity class
+//Enum through the differnt types of emotions to be used in the Activity class
+//
 enum Emotion {
 	HAPPINESS,
 	MOTIVATION,
@@ -23,15 +24,23 @@ enum Emotion {
 
 class Activity {
 public:
+
+	//Name of the activity
 	std::string name;
+
+	//Map associating emotions with their respective levels for the activity
 	std::map<Emotion, int> emotionLevels;
+
+	//harm level of the activity
 	int harmLevel;
+
+	//duration of the activity
 	int duration;
 
 
 
 
-	//constructor
+	//constructor that initializes the activity with the provided name, emotion level, harm level and duration
 	Activity(std::string name, std::map<Emotion, int> emotionLevels, int harmLevel, int duration);
 
 
