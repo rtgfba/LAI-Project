7b6a34661f760e30aeb39660f2930473931ae1ac
Remi Olateru <remiolateru@gmail.com>
2023-04-03 04:11:24 -0400

    Add comment to activity.cpp

--- activity.cpp
+++ activity.cpp
@@ -3,7 +3,7 @@
 #include "lifeai.h"
 
 
-
+//constructor activity which initializes the activity with the provided values
 Activity::Activity(std::string activityName, std::map<Emotion,int> activityEmotion, int activityHarmLevel, int activityDuration)
 	: name(activityName), emotionLevels(activityEmotion), harmLevel(activityHarmLevel), duration(activityDuration){
 
